527cd99af03a00494025a9dc3d42040e0a1231c5
michaelmathew <michael.mathew@stonybrook.edu>
2019-08-08 07:42:51 -0400

    Working locally with both api and php

---
+++ README.md
@@ -0,0 +1,114 @@
+# CLS
+CLS
+
+## CLS
+CLS
+
+
+
+
+
+## Advantages
+1.
+
+2.
+
+
+
+
+
+
+
+
+## Tools/Languages/References
+**Language:** 
+
+[Python3.6](https://www.python.org/)
+
+**Libraries:** 
+
+
+
+**OS Used:** 
+
+-Mac OS(Terminal)
+
+**Editors:** 
+-Visual Studio Code 
+
+
+**References**
+
+
+
+
+
+
+## Installation and Run 
+**Install:** 
+
+
+2.Install Tesseract OCR(Opticsl Character Recognition)
+
+```
+	brew install tesseract --HEAD
+```
+
+```
+	python3 -m pip install pytesseract
+```
+
+```
+	python3 -m pip install imutils
+```
+
+
+
+
+**Run:** 
+
+To start,
+
+```
+	n
+```
+
+
+
+
+
+
+
+## Functions
+
+CLS : n
+
+
+
+
+
+## Example
+
+CLS : n
+
+
+
+
+
+
+## Important Notes:
+
+**NOTE 1:** n
+
+
+
+
+
+##Common Errors
+
+1.bashprofile do-make sure evrything correct and then virtualenv module appears
+
+
+
+
+
---
+++ submit.php
@@ -0,0 +1,55 @@
+<?php
+if (!empty($_GET['location'])) {
+    /**
+     * Here we build the url we'll be using to access the google maps api
+     */
+    $users_url = 'http://127.0.0.1:5000/user/=' . $_GET['location'];
+       
+    $maps_json = file_get_contents($users_url);
+    #need to convert to php array
+    $users_array=json_decode($maps_json,true);
+
+/* 
+
+    $maps_array = json_decode($maps_json, true);
+    $lat = $maps_array['results'][0]['geometry']['location']['lat'];
+    $lng = $maps_array['results'][0]['geometry']['location']['lng'];
+   
+    $url = 'https://' .
+        'api.instagram.com/v1/media/search' .
+        '?lat=' . $lat .
+        '&lng=' . $lng .
+        '&client_id=CLIENT-ID'; //replace "CLIENT-ID"
+    $json = file_get_contents($url);
+    $array = json_decode($json, true);
+
+*/
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>geogram</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+<form action="" method="get">
+    <input type="text" name="location"/>
+    <button type="submit">Submit</button>
+</form>
+<br/>
+<div id="results" data-url="<?php if (!empty($url)) echo $url ?>">
+    <?php
+    if (!empty($array)) {
+        foreach ($array['data'] as $key => $item) {
+            echo '<img id="' . $item['id'] . '" src="' . $item['images']['low_resolution']['url'] . '" alt=""/><br/>';
+        }
+    }
+    ?>
+</div>
+</body>
+</html>
